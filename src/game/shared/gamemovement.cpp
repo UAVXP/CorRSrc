cfd14b102ba7b9d1c3c6fa42b85b5c4847424b69
UAVXP <uavxp29@gmail.com>
2013-02-10 18:19:55 +0200

    Fixed wall boosting

--- src/game/shared/gamemovement.cpp
+++ src/game/shared/gamemovement.cpp
@@ -1824,7 +1824,8 @@ void CGameMovement::Accelerate( Vector& wishdir, float wishspeed, float accel )
 		return;
 
 	// See if we are changing direction a bit
-	currentspeed = mv->m_vecVelocity.Dot(wishdir);
+//	currentspeed = mv->m_vecVelocity.Dot(wishdir);
+	currentspeed = sqrt( DotProduct(mv->m_vecVelocity, mv->m_vecVelocity) ); //VXP: Wall Boosting Exploit Fix
 
 	// Reduce wishspeed by the amount of veer.
 	addspeed = wishspeed - currentspeed;
